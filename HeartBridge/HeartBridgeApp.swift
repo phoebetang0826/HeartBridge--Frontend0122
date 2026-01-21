4f558ef164aec368dfdd3274ee9a933e4e019d51
Phoebe Tang <phoebetang0826@gmail.com>
2026-01-21 17:37:17 -0500

    Initial Commit

---
+++ HeartBridge/HeartBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBridgeApp.swift
+//  HeartBridge
+//
+//  Created by Phoebe Tang on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct HeartBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
